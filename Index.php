8c09889b465378c154e29edf6fe1d6cd287f83c0
Fahad Ashiq <44393684+fahadashiq12@users.noreply.github.com>
2021-03-10 12:53:20 +0500

    Few Changes

---
+++ Index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title> Theme for PHP </title>
+    <link rel="stylesheet" href="Style.css">
+</head>
+<body>
+
+
+
+<p class="para_style">  My name is Fahad </p>
+
+
+
+</body>
+</html>
